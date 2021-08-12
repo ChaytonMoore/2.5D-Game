aa8ff7f6fc69b8647f4e1a6d84b9ddb88322e9e7
ChaytonMoore <55141723+ChaytonMoore@users.noreply.github.com>
2021-08-12 17:29:31 +0100

    Update Files.cpp

--- Files.cpp
+++ Files.cpp
@@ -1,50 +1,31 @@
-#include "Data.h"
+#include "files.h"
 
-
-
-Line::Line(float a, float b, float c, float d)
+std::vector<Line> LoadWallsText(std::string File)
 {
-	Side1 = sf::Vector2f(a, b);
-	Side2 = sf::Vector2f(c, d);
-}
-
-Line::Line(sf::Vector2f a, float b, float c)
-{
-	Side1 = a;
-	Side2 = sf::Vector2f(b,c);
-}
-
-Wall::Wall(int a, int b, int c, int d) : Line(0, 0, 0, 0) 
-{
-	
-	Side1 = sf::Vector2f(a, b);
-	Side2 = sf::Vector2f(c, d);
-}
+	std::vector<Line> Output;
 
 
+	std::string FileData;
+	std::ifstream Data(File);
 
-float Distance(sf::Vector2f a, sf::Vector2f b)
-{
-	return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
-}
+	std::vector<std::string> LineData;
 
-float TaxiDistance(sf::Vector2f a, sf::Vector2f b)
-{
-	return abs(a.x - b.x) + abs(a.y + b.y);
-}
+	int a, b, c, d;
+	std::string::size_type sz;
 
-std::vector<std::string> SplitString(const std::string& str, const std::string& delim)
-{
-	std::vector<std::string> tokens;
-	size_t prev = 0, pos = 0;
-	do
+	while (std::getline(Data, FileData))
 	{
-		pos = str.find(delim, prev);
-		if (pos == std::string::npos) pos = str.length();
-		std::string token = str.substr(prev, pos - prev);
-		if (!token.empty()) tokens.push_back(token);
-		prev = pos + delim.length();
-	} while (pos < str.length() && prev < str.length());
-	return tokens;
-}
+		LineData = SplitString(FileData,",");
 
+		a = std::stoi(LineData[0], &sz);
+		b = std::stoi(LineData[1], &sz);
+		c = std::stoi(LineData[2], &sz);
+		d = std::stoi(LineData[3], &sz);
+		Output.push_back(Line(a,b,c,d));
+	}
+
+
+	Data.close();
+
+	return Output;
+}
